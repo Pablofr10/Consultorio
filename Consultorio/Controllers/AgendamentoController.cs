38383b14f7036a57c0ed3b30329d6f8aeb4cd80b
Pablorf10 <rafanet.live@gmail.com>
2021-09-16 21:28:54 -0300

    HTTP, StatusCode, Criando controllers

---
+++ Consultorio/Controllers/AgendamentoController.cs
@@ -0,0 +1,41 @@
+﻿using Consultorio.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Consultorio.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AgendamentoController : ControllerBase
+    {
+        List<Agendamento> agendamentos = new List<Agendamento>();
+        public AgendamentoController()
+        {
+            agendamentos.Add(new Agendamento
+            {
+                Id = 1,
+                NomePaciente = "Arian",
+                Horario = new DateTime(2021, 03, 16)
+            });
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(agendamentos);
+        }
+
+        [HttpGet("busca-agendamento/{id}")]
+        public IActionResult GetById(int id)
+        {
+            var agendamentoSelecionado = agendamentos.FirstOrDefault(x => x.Id == id);
+
+            return agendamentoSelecionado != null
+                ? Ok(agendamentoSelecionado)
+                : BadRequest("Erro ao buscar o agendamento");
+        }
+    }
+}
---
+++ Consultorio/Models/Entities/Agendamento.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Consultorio.Models.Entities
+{
+    public class Agendamento
+    {
+        public int Id { get; set; }
+        public string NomePaciente { get; set; }
+        public DateTime Horario { get; set; }
+    }
+}
