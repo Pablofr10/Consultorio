65c6cccbb89ad9d41ee045ffbdf1922ed6614840
Pablorf10 <rafanet.live@gmail.com>
2021-09-22 21:55:31 -0300

    Introdução ao Entity Framework

---
+++ Consultorio/Context/ConsultorioContext.cs
@@ -0,0 +1,13 @@
+﻿using Consultorio.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Consultorio.Context
+{
+    public class ConsultorioContext : DbContext
+    {
+        public ConsultorioContext(DbContextOptions<ConsultorioContext> options) : base(options)
+        {}
+
+        public DbSet<Agendamento> Agendamentos { get; set; }
+    }
+}
